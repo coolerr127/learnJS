65c57ebc71a2a13d274e50c2180e48c1557e0956
Anton <ashimanovich127@gmail.com>
2020-04-14 17:05:03 +0300

    Apr 14 2020 17:05

--- script1.js
+++ script1.js
@@ -16,10 +16,11 @@ for (let i = 0; i < 2; i++) {
         b = +prompt("Во сколько обойдется?", '');
     if (typeof(a) === 'string' &&
         typeof(a) != null && typeof(b) != null &&
-        a != '' && b != '' && 
-        a.length < 50) {
-        appData.expenses[a] = b;
+        a != '' && b != '' && a.length < 50) {
+            console.log ("done");
+            appData.expenses[a] = b;
     } else {
+        console.log ("bad result");
         i--;
     }
 }
