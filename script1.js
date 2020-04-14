a8e9896bc09f5e8fbee117669db49330a87ba5e0
Anton <ashimanovich127@gmail.com>
2020-04-14 16:59:41 +0300

    1

---
+++ script1.js
@@ -0,0 +1,38 @@
+"use strict";
+
+let money = +prompt('Ваш бюджет на месяц?', '');
+let date = prompt('Введите дату', '');
+let appData = {
+    budget : money,
+    timeData : date,
+    expenses : {},
+    optionalExepses : {},
+    inocme : [],
+    savings: false
+};
+    
+for (let i = 0; i < 2; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+        b = +prompt("Во сколько обойдется?", '');
+    if (typeof(a) === 'string' &&
+        typeof(a) != null && typeof(b) != null &&
+        a != '' && b != '' && 
+        a.length < 50) {
+        appData.expenses[a] = b;
+    } else {
+        i--;
+    }
+}
+
+appData.moneyPerDay = appData.budget / 30;
+alert('Ежедневный бюджет: ' + appData.moneyPerDay);
+
+if (appData.moneyPerDay < 100) {
+    console.log("Минимальный уровень достатка");
+}   else if (appData.moneyPerDay < 2000) {
+        console.log("Средний уровень достатка");
+}   else if (appData.moneyPerDay >= 2000) {
+        console.log("Высокий уровень достатка");
+}   else {
+        console.log("Error");
+}
